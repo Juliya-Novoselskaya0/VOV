49de24d623260b00d27ab49c71a7e523e80c9905
Juliya <57349170+Juliya-Novoselskaya0@users.noreply.github.com>
2020-02-17 19:52:25 +0300

    Add files via upload

--- search/GH-master/php/show_region.php
+++ search/GH-master/php/show_region.php
@@ -5,7 +5,7 @@
 	$coords_array = explode(';', $row['anim_coords']);
 	echo '
 	<img src='.$row["region_bigMap"].' class="icon" style="right:0px; bottom:0px; top:0px; left:0px">
-	<img src='.$row["animation"].' class="icon_anim" style="right:0px; bottom:0px; top:'.$coords_array[1].'px; left:'.$coords_array[0].'px">';
+	<img src='.$row["animation"].' class="icon_anim" style="width:15%; right:0px; bottom:0px; top:'.$coords_array[1].'px; left:'.$coords_array[0].'px">';
 	/*echo '
 	<img src="images\events_border\GrodnoRegion.png" class="icon" style="right:0px; bottom:0px; top:443px; left:-5px">
 	<img src="images\events_border\ShuchinRegion.png" class="icon" style="right:0px; bottom:0px; top:425px; left:200px">
--- search/GH-master/php/ssd.php
+++ search/GH-master/php/ssd.php
@@ -3,15 +3,17 @@
 	$res = mysqli_query($connection,'SELECT coords,map_image FROM places WHERE id_place='.$_POST['i_id']);
 	$row = mysqli_fetch_array($res);
 	$coords_array = explode(';', $row['coords']);
-	/*echo '
-	<img src='.$row["map_image"].' class="icon" style="right:0px; bottom:0px; top:'.$coords_array[1].'px; left:'.$coords_array[0].'px">
-	';*/
 	echo '
+	<img src='.$row["map_image"].' class="icon" style="right:0px; bottom:0px; top:'.$coords_array[1].'px; left:'.$coords_array[0].'px">
+	';
+	/*echo '
 	<img src="images\events_border\GrodnoRegion.png" class="icon" style="right:0px; bottom:0px; top:443px; left:-5px">
 	<img src="images\events_border\ShuchinRegion.png" class="icon" style="right:0px; bottom:0px; top:425px; left:200px">
 	<img src="images\events_border\BerestovicaRegion.png" class="icon" style="right:0px; bottom:0px; top:669px; left:53px">
 	<img src="images\events_border\MostyRegion.png" class="icon" style="right:0px; bottom:0px; top:617px; left:133px">
-	';
+	<img src="images\events_border\VolkovyskRegion.png" class="icon" style="right:0px; bottom:0px; top:680px; left:140px">
+	<img src="images\events_border\SvislochRegion.png" class="icon" style="right:0px; bottom:0px; top:812px; left:100px">
+	';*/
 	mysqli_free_result($res);
 	mysqli_close($connection);
 ?>
